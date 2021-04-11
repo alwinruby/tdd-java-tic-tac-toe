0cd068666b97fb8e8ff0d48f33ee6b9c300ec4e0
alwinruby <alwinsolanky@gmail.com>
2021-04-11 07:58:32 +0100

    Not quite started yet

--- src/test/java/tictactoe/TicTacToeSpec.java
+++ src/test/java/tictactoe/TicTacToeSpec.java
@@ -1,4 +1,24 @@
 package tictactoe;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 public class TicTacToeSpec {
+
+    @ExtendWith
+    public ExpectedException exception = ExpectedException.none();
+    private TicTacToe ticTacToe;
+
+    @BeforeEach
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+    @Test
+    public void whenXOutsideBoardThenRuntimeException()
+    {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
+
 }
